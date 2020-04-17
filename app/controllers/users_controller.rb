550d0374c6603891208fe96158986cfed6990e81
kelly_maass <kmaass77627@gmail.com>
2020-04-17 03:28:04 -0500

    first commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+
+
+    def index
+
+    end 
+
+    def new 
+
+    end 
+
+    def create
+
+    end
+
+
+    def show
+        @user = User.find(params[:id])
+      end
+
+
+
+
+
+
+
+
 end
